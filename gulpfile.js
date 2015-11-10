72cf23091b3c5510db3baa7930fb3a3387f24fc3
exegoist <exegoist@icloud.com>
2015-11-10 21:37:48 +0200

    overall

--- gulpfile.js
+++ gulpfile.js
@@ -29,8 +29,8 @@ gulp.task('jade', function(){
 gulp.task('scripts', function(){
   return gulp.src([
     './bower_components/jquery/dist/jquery.js',
-    './bower_components/foundation/js/foundation.js',
     './bower_components/jquery-mousewheel/jquery.mousewheel.js',
+    './bower_components/foundation/js/foundation.js',
     './assets/scripts/app.js'
   ])
   .pipe(concat('app.js'))
