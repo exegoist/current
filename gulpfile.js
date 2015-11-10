a3e9cb5e13cd8a6f373bb2a29a206dccc5537863
exegoist <exegoist@icloud.com>
2015-11-10 15:54:06 +0200

    gulp automatisation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var jade = require('gulp-jade');
 
 gulp.task('styles', function(){
   return gulp.src([
@@ -12,11 +13,32 @@ gulp.task('styles', function(){
     ]
   }))
   .pipe(concat('app.css'))
-  .pipe(gulp.dest('./public/css'));
+  .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('jade', function(){
+  return gulp.src([
+    './assets/**/*.jade'
+  ])
+  .pipe(jade())
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src([
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/foundation/js/foundation.js',
+    './bower_components/jquery-mousewheel/jquery.mousewheel.js',
+    './assets/scripts/app.js'
+  ])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('watch', function(){
   gulp.watch(['./assets/styles/**/*.sass', './assets/styles/**/*.scss'], ['styles']);
+  gulp.watch('./assets/**/*.jade', ['jade']);
+  gulp.watch('./assets/**/*.js', ['scripts']);
 });
 
 gulp.task('default', ['watch']);
