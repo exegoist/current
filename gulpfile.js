ab621262b017d6085ba6e91b46977086968f887b
exegoist <exegoist@icloud.com>
2015-11-12 18:11:11 +0200

    move to bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var jade = require('gulp-jade');
+var gls = require('gulp-live-server');
+
 
 gulp.task('styles', function(){
   return gulp.src([
@@ -9,7 +11,7 @@ gulp.task('styles', function(){
   ])
   .pipe(sass({
     includePaths: [
-      './bower_components/foundation/scss',
+      './bower_components/bootstrap-sass/assets/stylesheets',
       './bower_components/fullpage.js'
     ]
   }))
@@ -31,8 +33,7 @@ gulp.task('scripts', function(){
   return gulp.src([
     './bower_components/jquery/dist/jquery.js',
     './bower_components/fullpage.js/jquery.fullPage.js',
-    './bower_components/foundation/js/foundation/foundation.js',
-    './bower_components/foundation/js/foundation/foundation.topbar.js',
+    './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
     './assets/scripts/app.js'
   ])
   .pipe(concat('app.js'))
@@ -48,10 +49,20 @@ gulp.task('images', function(){
   .pipe(gulp.dest('./build/img'));
 });
 
-gulp.task('watch', function(){
+
+
+gulp.task('watch', ['styles','scripts','images','jade'], function(){
   gulp.watch(['./assets/styles/**/*.sass', './assets/styles/**/*.scss'], ['styles']);
   gulp.watch('./assets/**/*.jade', ['jade']);
   gulp.watch('./assets/scripts/**/*.js', ['scripts']);
 });
 
-gulp.task('default', ['styles','scripts','images','jade','watch']);
+gulp.task('serve', function(){
+  var server = gls.static('./build', 8080);
+  server.start();
+  gulp.watch(['./build/**/*.html','./build/**/*.css','./build/**/*.js'], function(file){
+    server.notify.apply(server, [file]);
+  });
+});
+
+gulp.task('default', ['serve', 'watch']);
